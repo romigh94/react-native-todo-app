4ff04c6d66b47615caa0b45929683627e2c8af48
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-05-16 14:44:16 +0200

    fetch done

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,53 @@
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import axios from "axios";
 
 export default function App() {
+
+const [todo, setTodo] = useState([])
+
+useEffect(() => {
+  fetchData();
+}, [])
+
+
+const fetchData = () => {
+
+  axios.get("http://localhost:5000/get")
+  .then(res => setTodo(res.data))
+  .catch(error => console.log(error))
+}
+
+
   return (
     <View style={styles.container}>
-      <Text>hello</Text>
-      <Button title="-->" />
       <StatusBar style="auto" />
+
+        <View style={styles.todo}>
+          <Text style={styles.todoText}>
+            <h2>To do</h2>
+          </Text>
+          <FlatList 
+            data={todo}
+            renderItem={({ item }) => <Text>{item.task}</Text>}
+            keyExtractor={(item) => item.id}
+          />
+        </View>
+
+        <View style={styles.inprogress}>
+          <Text>
+            <h2>In progress</h2>
+          </Text>
+
+        </View>
+
+        <View style={styles.done}>
+          <Text>
+            <h2>Done</h2>
+          </Text>
+        </View>
+
     </View>
   );
 }
@@ -18,4 +59,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  todo: {
+    backgroundColor: 'lightgreen',
+    width: 200,
+    height: 200
+  },
+  inprogress: {
+    backgroundColor: 'pink',
+    width: 200,
+    height: 200
+  },
+  done: {
+    backgroundColor: 'lightblue',
+    width: 200,
+    height: 200
+  }
 });
