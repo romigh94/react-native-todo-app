6eaebf245015a0118a7c7c8c5e5e7b6c710e4faf
Romina Ghaderi <romina.ghaderi@outlook.com>
2022-05-26 22:41:59 +0200

    changes

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,140 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, PanResponder, TextInput, Animated } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function Todo({navigation}) {
+
+  const [search, setSearch] = useState('')
+  const [tasks, setTasks] = useState([])
+  const [filteredTasks, setfilteredTasks] = useState([])
+
+
+useEffect(() => {
+  fetch("http://192.168.1.232:5000/get")
+  .then((response) => response.json())
+  .then((responseJson) => {
+    setTasks(responseJson);
+    setfilteredTasks(responseJson);
+  })
+  .catch((error) => {
+    console.error(error);
+  })
+}, []);
+
+  const searchFilter = (text) => {
+    if (text) {
+    const newfilteredTasks = tasks.filter(
+      (item) => {
+        const itemData = item.task ? item.task : ''
+        const textData = text;
+        return itemData.indexOf(textData) > -1;
+      })
+      setfilteredTasks(newfilteredTasks)
+      setSearch(text);
+    } else {
+      setfilteredTasks(tasks)
+      setSearch(text)
+    }
+  }
+  
+  
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.taskview}>
+          <Text style={styles.items} 
+          onPress={() => navigation.navigate('InProgress', {item: item.task})}>{item.task}</Text>
+      </TouchableOpacity>
+    )
+  }
+
+
+  return (
+    <View style={styles.container}>
+        <TextInput 
+          style={styles.textinput} 
+          placeholder="Filtrate tasks here..." 
+          onChangeText={(text) => searchFilter(text)} 
+          value={search} 
+          />
+
+        <View style={styles.innerContainers}>
+          <Text style={styles.Title}>
+            TASKS
+          </Text>
+          <FlatList 
+            data={filteredTasks}
+            renderItem={renderItem}
+          />
+        </View>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+function InProgress({navigation, route}) {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity>
+      <Text onPress={() => navigation.navigate('Done', {item: route.params.item})}>{route.params.item}</Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+function Done({navigation, route}) {
+  return (
+    <View style={styles.container}>
+      <Text>{route.params.item}</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Todo" component={Todo} />
+        <Stack.Screen name="InProgress" component={InProgress} />
+        <Stack.Screen name="Done" component={Done} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 30
+  },
+  Title: {
+    fontSize: 20,
+    padding: 5
+  },
+  textinput: {
+    padding: 8,
+    borderWidth: 1,
+    margin: 5,
+    width: 300
+  },
+  items: {
+    padding: 5
+  },
+})
+
+
+/*import React, {useState, useEffect, useRef} from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
 
 export default function App() {
 
@@ -9,7 +143,6 @@ const [tasks, setTasks] = useState([])
 const [filteredTasks, setfilteredTasks] = useState([])
 
 const pan = useRef(new Animated.ValueXY()).current;
-
 
 
 useEffect(() => {
@@ -30,8 +163,7 @@ const searchFilter = (text) => {
   if (text) {
   const newfilteredTasks = tasks.filter(
     (item) => {
-      const itemData = item.task
-      ? item.task : ''
+      const itemData = item.task ? item.task : ''
       const textData = text;
       return itemData.indexOf(textData) > -1;
     })
@@ -43,58 +175,15 @@ const searchFilter = (text) => {
   }
 }
 
-const panResponder = useRef(
-  PanResponder.create({
-    // Ask to be the responder:
-    onStartShouldSetPanResponder: (evt, gestureState) => true,
-    onStartShouldSetPanResponderCapture: (evt, gestureState) =>
-      true,
-    onMoveShouldSetPanResponder: (evt, gestureState) => true,
-    onMoveShouldSetPanResponderCapture: (evt, gestureState) =>
-      true,
-
-    onPanResponderGrant: (evt, gestureState) => {
-      // The gesture has started. Show visual feedback so the user knows
-      // what is happening!
-      // gestureState.d{x,y} will be set to zero now
-    },
-    onPanResponderMove: (evt, gestureState) => {
-      console.log(gestureState.moveY)
-      Animated.event([{y: pan.y}])({y: gestureState.moveY})
-      //Mapping gestureState.moveY that is equal to pan.y
-    },
-    onPanResponderTerminationRequest: (evt, gestureState) =>
-      false, 
-      // False - för att den inte ska bli avbruten när man drag & drops. Om det är på true, så kan den avbrytas tidigare.
-    onPanResponderRelease: (evt, gestureState) => {
-      // The user has released all touches while this view is the
-      // responder. This typically means a gesture has succeeded
-    },
-    onPanResponderTerminate: (evt, gestureState) => {
-      // Another component has become the responder, so this gesture
-      // should be cancelled
-    },
-    onShouldBlockNativeResponder: (evt, gestureState) => {
-      // Returns whether this component should block native components from becoming the JS
-      // responder. Returns true by default. Is currently only supported on android.
-      return true;
-    }
-  })
-).current;
-
-const reset = () => {
-  
-}
-
 
 const renderItem = ({ item }) => {
   return (
-<View style={styles.renderItemView}  {...panResponder.panHandlers}>
-    <View> 
-    <Text style={styles.DragandDropIcon}>@</Text> 
+    <View style={styles.renderItemView}  {...panResponder.panHandlers}>
+        <View> 
+        <Text style={styles.DragandDropIcon}>@</Text> 
+        </View>
+        <Text style={styles.items}>{item.task}</Text>
     </View>
-    <Text style={styles.items}>{item.task}</Text>
-</View>
   )
 }
 
@@ -102,20 +191,6 @@ const renderItem = ({ item }) => {
 
     <View style={styles.container}>
       <StatusBar style="auto" />
-
-      <Animated.View style={{
-          backgroundColor: 'black',
-          zIndex: 2,
-          height: 20,
-          width: "100%",
-          top: pan.getLayout().top
-        }}>
-          {renderItem}
-        </Animated.View>
-      
-      <Text style={styles.headline}>
-        To do app
-      </Text>
 
         <TextInput 
           style={styles.textinput} 
@@ -186,3 +261,5 @@ const styles = StyleSheet.create({
     padding: 5
   }
 });
+
+*/
